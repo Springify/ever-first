88bfce94debbfbf9f0d070776eca57e08c6467e2
Lorenzo Iraj Mendoza <lorenzo.iraj@gmail.com>
2020-10-29 21:22:15 +0800

    Recompile android app

--- android/app/src/main/java/com/limendoza/everfirst/MainActivity.java
+++ android/app/src/main/java/com/limendoza/everfirst/MainActivity.java
@@ -7,9 +7,6 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-import com.jeep.plugin.capacitor.CapacitorDataStorageSqlite;
-import com.byteowls.capacitor.filesharer.FileSharerPlugin;
-
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -19,14 +16,6 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-      add(CapacitorDataStorageSqlite.class);
-      add(FileSharerPlugin.class);
     }});
-  }
-
-  @Override
-  public void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    outState.clear();
   }
 }
